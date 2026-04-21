ef3e4e09544301dd4b3fc9bc458a7fbcac54a922
Rahul Kumar Yadav <dristidigitalagency@gmail.com>
2026-04-21 09:58:27 +0545

    added force-dynamic in products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import { ProductCard } from "@/components/ProductCard";
 import { CategoryFilter } from "@/components/CategoryFilter";
